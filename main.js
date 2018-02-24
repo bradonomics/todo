199d9518262a6098db2b8a8486b0e4f3c9e4a60d
Riolan <riolaney@att.net>
2018-02-24 15:33:48 -0600

    Add files via upload
    Uploading test files

---
+++ main.js
@@ -0,0 +1,13 @@
+function scheduleAdd(){
+	nameT = document.getElementById('task').value;
+	minutes = document.getElementById('min').value;
+	urgent = document.getElementById('urgent').value;
+	
+	document.getElementById('moodForm').reset();
+	holder = document.getElementById('holder');
+	holder.innerHTML = "";
+	
+}
+function planForm(){
+	
+}
